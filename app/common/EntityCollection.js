c8201ed5699d0a485f0a2e642001c0c690162283
weigang <weigangf@yonyou.com>
2019-08-27 10:09:28 +0800

    将所有的model  加载到内存中

--- app/common/EntityCollection.js
+++ app/common/EntityCollection.js
@@ -1,17 +1,16 @@
 'use strict';
-var EntityClass = require('./Entity');
+const EntityClass = require('./Entity')
+const _entityCollection = {}
 
-var _entityCollection = {};
-
+//将所有的model  加载到内存中
 exports.addEntity = function(entityName, name, parentEntity, parentField){
     var newEntity = new EntityClass(entityName, name, parentEntity, parentField);
     _entityCollection[entityName] = newEntity;
     // console.log(newEntity instanceof EntityClass);
-    console.log(_entityCollection);
     return newEntity;
 
 };
-
+//通过entityName 获取mongoose 对象
 exports.getEntity = function(entityName){
     var entity = _entityCollection[entityName];
     if(entity == undefined || entity == null){
